6211f0ddb53de6be420a3623f542a758826dfc03
matrixbirds <yooobuntu@gmail.com>
2019-08-08 17:23:21 +0800

    upgrade to 0.59.10

--- android/app/src/main/java/com/agorarnquickstart/MainApplication.java
+++ android/app/src/main/java/com/agorarnquickstart/MainApplication.java
@@ -3,9 +3,6 @@ package com.agorarnquickstart;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.syan.agora.AgoraPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new AgoraPackage(),
-            new RNGestureHandlerPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
 
